ee368edf900f2777613447d7e311e52afe973243
nvphuc <Phuc.NguyenVan@nttdata.com>
2017-11-02 14:09:50 +0700

    show data on html

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,18 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 
+import {Hero} 				from '../model/hero';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+	heroes: Hero[];
+	constructor() {
 
-  constructor() {
-	console.log('Component initialised!');
-  }
+	}
 
-  ngOnInit() {
-	console.log('Component initialised!');
-  }
+	ngOnInit() {
+		this.heroes = [
+      { id: 0,  name: 'Zero' },
+      { id: 11, name: 'Mr. Nice' },
+      { id: 12, name: 'Narco' },
+      { id: 13, name: 'Bombasto' },
+      { id: 14, name: 'Celeritas' },
+      { id: 15, name: 'Magneta' },
+      { id: 16, name: 'RubberMan' },
+      { id: 17, name: 'Dynama' },
+      { id: 18, name: 'Dr IQ' },
+      { id: 19, name: 'Magma' },
+      { id: 20, name: 'Tornado' }
+    ];
+	}
 
 }
